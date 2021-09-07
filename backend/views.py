48ea82a58750aac5a24a70e6d87e177e8b6038e1
Ferlita Halim <ferlitahalim@gmail.com>
2021-09-07 19:05:25 +0800

    init repo

---
+++ backend/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+# Create your views here.
+
+
+def hello_world(request):
+    return JsonResponse({'message': f'hello'}, status=200)
+    # return "hello world"
+
+
+def hello_world_name(request, name):
+    return JsonResponse({'message': f'hello, {name}'}, status=200)
+    # return "hello world" + name
