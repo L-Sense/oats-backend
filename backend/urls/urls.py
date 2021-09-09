43621aab41388164aec90d5f74273b854532fa3d
L-Irvin <leonardoirvinp@gmail.com>
2021-09-09 19:25:45 +0800

    create endpoints for populating and truncating databases

--- backend/urls/urls.py
+++ backend/urls/urls.py
@@ -3,5 +3,6 @@ from backend.views import views, populate
 
 urlpatterns = [
     path('', views.hello_world, name="hello_world"),
-    path('populate/', populate.populate_everything, name="populate")
+    path('populate/', populate.populate_everything, name="populate"),
+    path('truncate/', populate.truncate_everything, name="truncate")
 ]
--- backend/views/populate.py
+++ backend/views/populate.py
@@ -1,17 +1,29 @@
-import json
+from django.http import JsonResponse
 
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from backend.models import *
 
-from backend.models import Department
-
-# Create your views here.
+def image_to_bytea(image):
+    with open(image, "rb") as img:
+        f = img.read()
+        b = bytearray(f)
+        return b
 
 def populate_everything(request):
     department_one = Department.objects.create(department_name='SCSE', late_threshold=15)
     department_two = Department.objects.create(department_name='SPMS', late_threshold=15)
-    return HttpResponse(json.dumps({
+    employee_one = Employee.objects.create(employee_name='Koh Boon Juey', department_id=1, image_1=image_to_bytea("backend/views/U1921258H_2.jpg"))
+    employee_two = Employee.objects.create(employee_name='Leonardo Irvin Pratama', department_id=1, image_1=image_to_bytea("backend/views/U1920301J_1.jpg"))
+    attendance_one = Attendance.objects.create(flag=False, type=True, employee_id=1)
+    # TO-DO: I'm not sure what type of hashing you would want to implement here
+    return JsonResponse({
         'message': 'Data inserted successfully.'
-    }), status=200)
+    }, status=200)
 
-# run TRUNCATE TABLE backend_department RESTART IDENTITY CASCADE; to reset the data and increment
+def truncate_everything(request):
+    truncate_attendance = Attendance.objects.all().delete()
+    truncate_employee = Employee.objects.all().delete()
+    truncate_department = Department.objects.all().delete()
+    truncate_admin = Admin.objects.all().delete()
+    return JsonResponse({
+        'message': 'Tables truncated successfully.'
+    }, status=200)
