dd0eeeb94fdb2015d1905d294711cb1ca47c6d42
Ferlita Halim <ferlitahalim@gmail.com>
2021-10-14 01:16:24 +0800

    fix status codes of scanner APIs

--- backend/views/scanner.py
+++ backend/views/scanner.py
@@ -14,40 +14,49 @@ from datetime import datetime, date, time
 
 from deepface import DeepFace
 
+
 def parse(dir):
     return int(re.findall(r"_\w+_", dir)[0][1:-1])
+
 
 @api_view(['POST'])
 @token_required
 def scanner_photo(request):
-    image_parse = JSONParser().parse(request)
-    image_string = image_parse['image']
-    check = image_parse['isCheckin']
-    image = base64.b64decode(image_string)
+    try:
+        image_parse = JSONParser().parse(request)
+        image_string = image_parse['image']
+        check = image_parse['isCheckin']
+        image = base64.b64decode(image_string)
 
-    if os.path.exists("input"):
-        shutil.rmtree('input')
-    os.makedirs('input')
+        if os.path.exists("input"):
+            shutil.rmtree('input')
+        os.makedirs('input')
 
-    test_dir = os.path.join("input/", "test.jpg")
-    with open(test_dir, 'wb') as f:
-        f.write(image)
+        test_dir = os.path.join("input/", "test.jpg")
+        with open(test_dir, 'wb') as f:
+            f.write(image)
+    except:
+        return Response({
+            "message": "invalid data",
+            "data": [],
+        }, 500)
 
     try:
-        DeepFace.detectFace(test_dir) # detector_backend='dlib' but dlib ModuleNotFoundError
+        # detector_backend='dlib' but dlib ModuleNotFoundError
+        DeepFace.detectFace(test_dir)
     except ValueError:
         shutil.rmtree('input')
         return Response({
             "message": "face not detected",
-            "status": 500
-        })
+            "data": [],
+        }, 500)
 
     try:
         results = DeepFace.find(
-                test_dir,
-                db_path = "images",
-                distance_metric='euclidean_l2'
-                )
+            test_dir,
+            db_path="images",
+            distance_metric='euclidean_l2'
+        )
         employee = parse(results["identity"][0])
         employee_info = Employee.objects.get(pk=employee)
         shutil.rmtree('input')
@@ -55,81 +64,78 @@ def scanner_photo(request):
         if len(results) == 0:
             return Response({
                 "message": "face does not exist in our database",
-                "status": 500
-            })
+                "data": [],
+            }, 500)
 
         try:
-            attendance = Attendance.objects.filter(date=date.today()).get(employee=employee)
+            attendance = Attendance.objects.filter(
+                date=date.today()).get(employee=employee)
             if check:
                 if attendance.in_time:
                     return Response({
-                            "message": "attendance already recorded",
-                            "data":
-                                {
-                                    "id": employee,
-                                    "employee_name": employee_info.employee_name,
-                                    "department_id": employee_info.department_id,
-                                    "department_name": Department.objects.get(pk=employee_info.department_id).department_name
-                                },
-                            "status": 200
-                        })
+                        "message": "attendance already recorded",
+                        "data":
+                        {
+                            "id": employee,
+                            "employee_name": employee_info.employee_name,
+                            "department_id": employee_info.department_id,
+                            "department_name": Department.objects.get(pk=employee_info.department_id).department_name
+                        },
+                    }, 200)
                 else:
                     attendance.in_time = datetime.now().time()
-                    threshold = datetime.combine(date.today(), time(8,0,0))
+                    threshold = datetime.combine(date.today(), time(8, 0, 0))
                     diff = datetime.now() - threshold
-                    late = abs(diff.total_seconds())>900
+                    late = abs(diff.total_seconds()) > 900
                     if late:
                         attendance.status = "Abnormal"
                     attendance.save()
                     return Response({
-                            "message": "attendance saved",
-                            "data":
-                                {
-                                    "id": employee,
-                                    "employee_name": employee_info.employee_name,
-                                    "department_id": employee_info.department_id,
-                                    "department_name": Department.objects.get(pk=employee_info.department_id).department_name
-                                },
-                            "status": 200
-                        })
+                        "message": "attendance saved",
+                        "data":
+                        {
+                            "id": employee,
+                            "employee_name": employee_info.employee_name,
+                            "department_id": employee_info.department_id,
+                            "department_name": Department.objects.get(pk=employee_info.department_id).department_name
+                        },
+                    }, 200)
             else:
                 if attendance.out_time:
                     return Response({
-                            "message": "attendance already recorded",
-                            "data":
-                                {
-                                    "id": employee,
-                                    "employee_name": employee_info.employee_name,
-                                    "department_id": employee_info.department_id,
-                                    "department_name": Department.objects.get(pk=employee_info.department_id).department_name
-                                },
-                            "status": 200
-                        })
+                        "message": "attendance already recorded",
+                        "data":
+                        {
+                            "id": employee,
+                            "employee_name": employee_info.employee_name,
+                            "department_id": employee_info.department_id,
+                            "department_name": Department.objects.get(pk=employee_info.department_id).department_name
+                        },
+                    }, 200)
                 else:
                     attendance.out_time = datetime.now().time()
-                    threshold = datetime.combine(date.today(), time(17,0,0))
+                    threshold = datetime.combine(date.today(), time(17, 0, 0))
                     diff = datetime.now() - threshold
-                    late = abs(diff.total_seconds())>900
+                    late = abs(diff.total_seconds()) > 900
                     if late:
                         attendance.status = "Abnormal"
                     attendance.save()
                     return Response({
-                            "message": "attendance saved",
-                            "data":
-                                {
-                                    "id": employee,
-                                    "employee_name": employee_info.employee_name,
-                                    "department_id": employee_info.department_id,
-                                    "department_name": Department.objects.get(pk=employee_info.department_id).department_name
-                                },
-                            "status": 200
-                        })
+                        "message": "attendance saved",
+                        "data":
+                        {
+                            "id": employee,
+                            "employee_name": employee_info.employee_name,
+                            "department_id": employee_info.department_id,
+                            "department_name": Department.objects.get(pk=employee_info.department_id).department_name
+                        },
+                    }, 200)
 
         except Attendance.DoesNotExist:
             if check:
-                threshold = datetime.combine(date.today(), time(8,0,0))
+                threshold = datetime.combine(date.today(), time(8, 0, 0))
                 diff = datetime.now() - threshold
-                late = abs(diff.total_seconds())>900
+                late = abs(diff.total_seconds()) > 900
                 status = "Abnormal" if late else "Normal"
                 attendance_data = {
                     "date": date.today(),
@@ -137,30 +143,30 @@ def scanner_photo(request):
                     "in_time": datetime.now().time(),
                     "status": status
                 }
-                attendance_serializer = AttendanceSerializer(data=attendance_data)
+                attendance_serializer = AttendanceSerializer(
+                    data=attendance_data)
                 if attendance_serializer.is_valid():
                     attendance_serializer.save()
                     return Response({
                         "message": "attendance saved",
                         "data":
-                            {
-                                "id": employee,
-                                "employee_name": employee_info.employee_name,
-                                "department_id": employee_info.department_id,
-                                "department_name": Department.objects.get(pk=employee_info.department_id).department_name
-                            },
-                        "status": 200
-                    })
+                        {
+                            "id": employee,
+                            "employee_name": employee_info.employee_name,
+                            "department_id": employee_info.department_id,
+                            "department_name": Department.objects.get(pk=employee_info.department_id).department_name
+                        },
+                    }, 200)
                 else:
                     return Response({
                         "message": "server error",
-                        "status": 500
-                    })
+                        "data": [],
+                    }, 500)
 
             else:
-                threshold = datetime.combine(date.today(), time(17,0,0))
+                threshold = datetime.combine(date.today(), time(17, 0, 0))
                 diff = datetime.now() - threshold
-                late = abs(diff.total_seconds())>900
+                late = abs(diff.total_seconds()) > 900
                 status = "Abnormal" if late else "Normal"
                 attendance_data = {
                     "date": date.today(),
@@ -168,7 +174,8 @@ def scanner_photo(request):
                     "out_time": datetime.now().time(),
                     "status": status
                 }
-                attendance_serializer = AttendanceSerializer(data=attendance_data)
+                attendance_serializer = AttendanceSerializer(
+                    data=attendance_data)
                 if attendance_serializer.is_valid():
                     attendance_serializer.save()
                     return Response({
@@ -180,17 +187,16 @@ def scanner_photo(request):
                                 "department_id": employee_info.department_id,
                                 "department_name": Department.objects.get(pk=employee_info.department_id).department_name
                             },
-                        "status": 200
-                    })
+                    }, 200)
                 else:
                     return Response({
                         "message": "server error",
-                        "status": 500
-                    })
+                        "data": []
+                    }, 500)
 
     except ValueError:
         shutil.rmtree('input')
         return Response({
             "message": "server fails to detect the image database, ask the admin to reload the database",
-            "status": 500
-        })
+            "data": [],
+        }, 500)
