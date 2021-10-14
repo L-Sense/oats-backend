54cd6cdab77f54a340fc00c743b88f75428181e0
boonjuey <50329640+boonjuey@users.noreply.github.com>
2021-10-14 23:02:08 +0800

    Add @token_required

--- backend/views/scanner.py
+++ backend/views/scanner.py
@@ -1,3 +1,4 @@
+from backend.decorators import token_required
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
